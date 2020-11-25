4ff7650d69d023c158def3dfc51875f5e81a4202
kshidara <kshidara@yahoo-corp.jp>
2020-11-25 22:35:46 +0900

    アイコン調整など

--- js/index.js
+++ js/index.js
@@ -1,9 +1,15 @@
 (function ($) {
   // メニューボタンを押した際の挙動
   $("#header__navigationButton").click(function () {
-    $("#header__navigationButton").toggleClass(
-      "header__navigationButton--close"
-    );
+    if ($("#header__navigationButton").hasClass("icon--menu")) {
+      $("#header__navigationButton")
+        .removeClass("icon--menu")
+        .addClass("icon--delete");
+    } else {
+      $("#header__navigationButton")
+        .removeClass("icon--delete")
+        .addClass("icon--menu");
+    }
     $(".navigation").toggleClass("navigation--show");
     $(".body").toggleClass("body--preventScroll");
     // 上記だとスクロール位置が失われるので対策必要
