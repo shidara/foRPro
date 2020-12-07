4cc8188c7a15ae9a80e6902c5846e486c8be0113
kshidara <kshidara@yahoo-corp.jp>
2020-12-07 22:36:35 +0900

    scroll animation

--- js/index.js
+++ js/index.js
@@ -1,17 +1,15 @@
 (function ($) {
+  // ロード時にキャッチコピーのフェードin
+  $("#section1 .catchCopy__text").animate(
+    {
+      opacity: "1",
+    },
+    "slow",
+    "swing"
+  );
+
   // メニューボタンを押した際の挙動
   $("#header__navigationButton").click(function () {
-    if ($("#header__navigationButton").hasClass("icon--menu")) {
-      // メニューを開いた時の挙動
-      $("#header__navigationButton")
-        .removeClass("icon--menu")
-        .addClass("icon--delete");
-    } else {
-      // メニューを閉じた時の挙動
-      $("#header__navigationButton")
-        .removeClass("icon--delete")
-        .addClass("icon--menu");
-    }
     $(".navigation").toggleClass("navigation--show");
     setTimeout(() => {
       $(".navigation .navigation__listItem").toggleClass(
@@ -21,6 +19,4 @@
     $(".body").toggleClass("body--preventScroll");
     // 上記だとスクロール位置が失われるので対策必要
   });
-
-  // $(".slick01").slick();
 })(jQuery);
