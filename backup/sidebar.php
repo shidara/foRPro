375dd8824fa9f9be848d0d2e880ddb8c0bbb7c54
kshidara <kshidara@yahoo-corp.jp>
2021-08-11 22:37:11 +0900

    backup

---
+++ backup/sidebar.php
@@ -0,0 +1,55 @@
+    <!-- navigation -->
+    <nav class="navigation">
+      <ul class="navigation__list">
+        <li class="navigation__listItem">
+          <div
+            class="navigation__listItemButton"
+            data-transform="3"
+            role="button"
+          >
+            プロの皆様へ
+          </div>
+        </li>
+        <li class="navigation__listItem">
+          <div
+            class="navigation__listItemButton"
+            data-transform="4"
+            role="button"
+          >
+            foRProの魅力
+          </div>
+        </li>
+        <li class="navigation__listItem">
+          <div
+            class="navigation__listItemButton"
+            data-transform="5"
+            role="button"
+          >
+            プロジェクト事例
+          </div>
+        </li>
+        <li class="navigation__listItem">
+          <div
+            class="navigation__listItemButton"
+            data-transform="6"
+            role="button"
+          >
+            ご利用フロー
+          </div>
+        </li>
+      </ul>
+
+      <div class="applicationForm applicationForm--navigation">
+        <div class="applicationForm__contents">
+          <a
+            href="/registration/"
+            class="applicationForm__button applicationForm__web"
+            >Web</a
+          ><a
+            href="/lineform/"
+            class="applicationForm__button applicationForm__line applicationForm__line--border"
+            >LINE</a
+          >
+        </div>
+      </div>
+    </nav>
